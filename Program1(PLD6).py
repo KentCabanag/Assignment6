e98967692fd98e6437dd2e58fc4de801a85b91f8
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-11-22 11:05:51 +0800

    commit

---
+++ Program1(PLD6).py
@@ -0,0 +1,14 @@
+#Program 1: Number Sorter
+#Create a program that ask 4 numbers. 
+#Print the 4 numbers from highest to lowest using only if-else statement.
+
+print("\n\033[32;1mArranging Four Numbers From Highest to Lowest\033[0m\n")
+
+num1 = "{:g}".format(float(input("Enter your First Number: ")))
+num2 = "{:g}".format(float(input("Enter your Second Number: ")))
+num3 = "{:g}".format(float(input("Enter your Third Number: ")))
+num4 = "{:g}".format(float(input("Enter your Fourth Number: ")))
+print(num1)
+
+
+
