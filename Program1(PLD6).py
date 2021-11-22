e811b9d0aaa9bef6cf846d35f879e34024a2197d
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-11-22 14:17:18 +0800

    commit

--- Program1(PLD6).py
+++ Program1(PLD6).py
@@ -11,16 +11,58 @@ num4 = "{:g}".format(float(input("\033[31;1mEnter your Fourth Number\033[0m: "))
 
 def Num_1():
     if num1 >= num2 >= num3 >= num4:
-        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num2}, {num3}, {num4}).")
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num2}, {num3}, {num4})")
     elif num1 >= num4 >= num2 >= num3:
-        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num4}, {num2}, {num3}).")
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num4}, {num2}, {num3})")
     elif num1 >= num3 >= num4 >= num2:
-        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num3}, {num4}, {num2}).")
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num3}, {num4}, {num2})")
     elif num1 >= num3 >= num2 >= num4:
-        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num3}, {num2}, {num4}).")
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num3}, {num2}, {num4})")
     elif num1 >= num4 >= num3 >= num2:
-        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num4}, {num3}, {num2}).")
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num4}, {num3}, {num2})")
     elif num1 >= num2 >= num4 >= num3:
-        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num2}, {num4}, {num3}).")
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num1}, {num2}, {num4}, {num3})")
+def Num_2():
+    if num2 >= num4 >= num3 >= num1:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num2}, {num4}, {num3}, {num1})")
+    elif num2 >= num4 >= num1 >= num3:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num2}, {num4}, {num1}, {num3})")
+    elif num2 >= num3 >= num4 >= num1:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num2}, {num3}, {num4}, {num1})")
+    elif num2 >= num3 >= num1 >= num4:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num2}, {num3}, {num1}, {num4})")
+    elif num2 >= num1 >= num4 >= num3:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num2}, {num1}, {num4}, {num3})")
+    elif num2 >= num1 >= num3 >= num4:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num2}, {num1}, {num3}, {num4})")
+def Num_3():
+    if num3 >= num4 >= num2 >= num1:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num3}, {num4}, {num2}, {num1})")
+    elif num3 >= num4 >= num1 >= num2:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num3}, {num4}, {num1}, {num2})")
+    elif num3 >= num2 >= num4 >= num1:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num3 }, {num2}, {num4}, {num1})")
+    elif num3 >= num2 >= num1 >= num4:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num3}, {num2}, {num1}, {num4})")
+    elif num3 >= num1 >= num2 >= num4:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num3}, {num1}, {num2}, {num4})")
+    elif num3 >= num1 >= num4 >= num2:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num3}, {num1}, {num4}, {num2})")
+def Num_4():
+    if num4 >= num3 >= num2 >= num1:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num4}, {num3}, {num2} {num1})")
+    elif num4 >= num3 >= num1 >= num2:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num4}, {num3}, {num1}, {num2})")
+    elif num4 >= num2 >= num3 >= num1:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num4}, {num2}, {num3}, {num1})")
+    elif num4 >= num2 >= num1 >= num3:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num4}, {num2}, {num1}, {num3})")
+    elif num4 >= num1 >= num2 >= num3:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num4}, {num1}, {num2}, {num3})")
+    elif num4 >= num1 >= num3 >= num2:
+        print(f"\033[34;4mHighest to Lowest\033[0m: ({num4}, {num1}, {num3}, {num2})")
 
 Num_1()
+Num_2()
+Num_3()
+Num_4()
