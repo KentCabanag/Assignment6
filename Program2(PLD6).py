a6dc1073f2493d20744ddbae3562d153d27d55b7
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-11-26 21:25:19 +0800

    commit

--- Program2(PLD6).py
+++ Program2(PLD6).py
@@ -3,3 +3,41 @@
 #Let the user answer and evaluate if the user has the correct answer
 #The program will ask the user 10 addition operation
 #Display the result summary of the 10 operations (ex 9/10)
+
+import random
+
+rand1 = random.randint(0,99)
+rand2 = random.randint(0,99)
+
+score = 10
+
+def questions():
+    print("Question Number 1")
+    print(f"What is {rand1} + {rand2}")
+    randAns = rand1 + rand2
+    userAns = int(input("Enter your Answer:"))
+    if randAns == userAns:
+        print("Correct")
+    elif userAns != randAns:
+        print(f"Incorrect, The Correct Answer is {randAns}.")
+        score - 1
+    print("Question Number 2")
+    print(f"What is {rand1} + {rand2}")
+    randAns = rand1 + rand2
+    userAns = int(input("Enter your Answer:"))
+    if randAns == userAns:
+        print("Correct")
+    elif userAns != randAns:
+        print(f"Incorrect, The Correct Answer is {randAns}.")
+        score - 1
+    print("Question Number 3")
+    print(f"What is {rand1} + {rand2}")
+    randAns = rand1 + rand2
+    userAns = int(input("Enter your Answer:"))
+    if randAns == userAns:
+        print("Correct")
+    elif userAns != randAns:
+        print(f"Incorrect, The Correct Answer is {randAns}.")
+        score - 1
+    
+questions()
